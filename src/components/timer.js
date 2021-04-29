f19d650e229fccef208367efc734087811d86039
sarahhsoltani <sarasoltani.simplon.enda@gmail.com>
2021-04-29 22:51:10 +0100

    done

--- src/components/timer.js
+++ src/components/timer.js
@@ -25,8 +25,10 @@ class Timer extends Component {
      }
    const inter= setInterval(
         () => {
+            
            this.setState({
-               s: this.state.s + 1
+             s: this.state.s + 1,
+           
            }) 
         },  1000
     )
@@ -56,35 +58,35 @@ PauseTime =()=> {
     })
 }
  render() {
-    console.log("time is",this.state.t);
+    
   return(
-   <Container>
-       <h1 className="mt-5 timer">Timer </h1>
-     <div className="d-flex justify-content-around mt-5">
+   <Container className="timee text-center  p-5 mt-5">
+       <h1 className="mt-2 timer">Type your Time please  <i class="far fa-grin-hearts"></i> </h1>
+     <div className="text-center ">
          <div >
-         <div className="input-group w-100  mt-5">
-            <span className="input-group-text" id="basic-addon1"><button className=" btn-small" onClick={this.convertTime}>convert</button></span>
-            <input  onChange={this.EvenTime} type="text" className="form-control  py-3 " placeholder="type your time please" aria-label="Username" aria-describedby="basic-addon1"/>
+         <div className="input-group w-50  input-time mt-5 ml-5">
+            <span className="input-group-text spann" id="basic-addon1"><button className=" btn-small" onClick={this.convertTime}>convert</button></span>
+            <input  onChange={this.EvenTime} type="text" className="form-control  py-4 " placeholder="type your time please" aria-label="Username" aria-describedby="basic-addon1"/>
           </div>
          </div>
-         <div>
-             <img src="./image/img.svg" className="ml-5" alt="..." />
-         </div>
+       
      </div>
-       <div className="time">
+       <div className="time text-center ">
        <h1 className="mr-4">{String(this.state.h).padStart(2,'0')}:</h1>
        <h1 className="mr-4">{String(this.state.m).padStart(2,'0')}:</h1>
        <h1 className="mr-4">{String(this.state.s).padStart(2,'0')}</h1>
        
        </div>
-       <div className="box-item">
+       <div className="box-item ">
         <span className="mr-4">Heures</span>
        <span className="mr-4">Minutes</span>
        <span className="mr-4">Secondes</span>
        </div>
-  <input onClick={this.RessetTime} type="button" value="Reset"/>
-  <input onClick={this.StartTime} type="button" value="Start"/>
-  <input onClick={this.PauseTime} type="button" value="Pause"/>
+        <div className="events mt-4">
+        <button onClick={this.RessetTime}  className="btn event">Reset <i class="fas fa-undo-alt"></i> </button>
+        <button onClick={this.StartTime}    className="btn event">Start <i class="fas fa-play"></i></button>
+        <button onClick={this.PauseTime}   className="btn event">Pause <i class="fas fa-pause"></i></button> 
+        </div>
    </Container>
     )
    }
